d3d458fc09ead97d9f6f233a4801e24a7d72d636
Fumihiko Sano <sano@itolator.co.jp>
2021-10-07 12:54:50 +0900

    日付の区切りとしてハイフンも可能にした

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -2,11 +2,11 @@ import moment from 'moment';
 
 export const datesToDue = (date: string) => {
   const today = moment();
-  
+
   // momentが解釈できる形に変換
-  if (date.match(/\d{4}[\/\.]\d+[\/\.]\d+/)) {      // yyyy/mm/dd or yyyy.mm.dd
+  if (date.match(/\d{4}[\/\.\-]\d+[\/\.\-]\d+/)) {    // yyyy-mm-dd or yyyy/mm/dd or yyyy.mm.dd
     date = date.replace(/[\/\.]/g, "-");
-  } else if (date.match(/\d+[\/\.]\d+/)) {          // mm/dd or mm.dd
+  } else if (date.match(/\d+[\/\.\-]\d+/)) {          // mm-dd or mm/dd or mm.dd
     date = today.year() + "-" + date;               // 年を追加
     date = date.replace(/[\/\.]/g, "-");
   } else {
