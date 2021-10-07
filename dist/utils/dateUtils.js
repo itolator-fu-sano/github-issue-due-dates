4fab1b10ff1f5dde77792cabc455d02a3b85197e
Fumihiko Sano <sano@itolator.co.jp>
2021-10-07 13:10:11 +0900

    コンパイル済みファイルの追加モレ

--- dist/utils/dateUtils.js
+++ dist/utils/dateUtils.js
@@ -8,10 +8,10 @@ const moment_1 = __importDefault(require("moment"));
 const datesToDue = (date) => {
     const today = (0, moment_1.default)();
     // momentが解釈できる形に変換
-    if (date.match(/\d{4}[\/\.]\d+[\/\.]\d+/)) { // yyyy/mm/dd or yyyy.mm.dd
+    if (date.match(/\d{4}[\/\.\-]\d+[\/\.\-]\d+/)) { // yyyy-mm-dd or yyyy/mm/dd or yyyy.mm.dd
         date = date.replace(/[\/\.]/g, "-");
     }
-    else if (date.match(/\d+[\/\.]\d+/)) { // mm/dd or mm.dd
+    else if (date.match(/\d+[\/\.\-]\d+/)) { // mm-dd or mm/dd or mm.dd
         date = today.year() + "-" + date; // 年を追加
         date = date.replace(/[\/\.]/g, "-");
     }
